0ac677c81d7c33203fe702dd3fc72cac76f53945
MarzQnx <marzuqnx@gmail.com>
2025-02-26 18:13:32 +0700

    update nickel-scrapper.php

--- nickel-scrapper.php
+++ nickel-scrapper.php
@@ -6,7 +6,7 @@
  * Version: 2.9
  * Text Domain: nickel-scrapper
  * Author: dbanie
- * Author URI: https://www.marzqnx.com
+ * Author URI: https://www.marzuqnx.com
  */
 
 // Include Composer autoload
